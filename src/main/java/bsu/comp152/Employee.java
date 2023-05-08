9c8010368c3f466f50c195cbd070821ce391df3a
lkgross <profgross@gmail.com>
2023-05-08 11:01:52 -0400

    Initial commit

---
+++ src/main/java/bsu/comp152/Employee.java
@@ -0,0 +1,12 @@
+package bsu.comp152;
+
+public class Employee {
+    // Define five instance variables.
+    private String name;
+    private int level;
+    private int yearsAtCompany;
+    private boolean worksNights;
+    private boolean worksExtraHours;
+
+
+}
